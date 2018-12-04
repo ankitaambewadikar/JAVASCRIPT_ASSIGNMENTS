0b41a6009ea54d61cd438076a0ace11b6fd54253
Ambewadikar <ankitaambewadikar@gmail.com>
2018-12-04 19:34:41 +0530

    Adds Javascript assignments of .docx file

---
+++ JavaScript_Assignments/MyCalculator/calculator.js
@@ -0,0 +1,19 @@
+function calculator(){
+
+    var text;
+    var menu = prompt("enter operation to perform \n 1)Add\n2)Substract\n3)Multiply\n4)Divide\n5)Exponent\n6)Mean\n7)Quit");
+   
+       if(menu<1 || menu >7){
+           alert("Error Select Again");
+
+       }
+       else{
+           add();
+       }
+}
+
+function add(){
+    n1=window.prompt("Enter first number");
+    n2=window.prompt("Enter second number");
+    sum=parseInt(n1,10)parseInt()
+}
