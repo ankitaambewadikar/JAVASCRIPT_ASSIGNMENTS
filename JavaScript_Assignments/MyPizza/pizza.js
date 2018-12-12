deecdbd7ef222c3f896d7cdcc7d176bf90d267f4
Ambewadikar <ankitaambewadikar@gmail.com>
2018-12-12 20:09:45 +0530

    Adds modified MyPizza files

---
+++ JavaScript_Assignments/MyPizza/pizza.js
@@ -0,0 +1,47 @@
+/*function createTable(){
+
+    var table = document.getElementById("myTable");
+        
+    var rowOne =  table.insertRow(0);
+   var colOne = rowOne.insertCell(0);
+   var colTwo = rowOne.insertCell(1);
+   colOne.innerHTML = "Name:";
+   colTwo.innerHTML = name;
+
+    var rowOne =  table.insertRow(1);
+   var colOne = rowOne.insertCell(0);
+   var colTwo = rowOne.insertCell(1);
+   colOne.innerHTML = "Email:";
+   colTwo.innerHTML = email;
+
+    var rowOne =  table.insertRow(2);
+   var colOne = rowOne.insertCell(0);
+   var colTwo = rowOne.insertCell(1);
+   colOne.innerHTML = "Address:";
+   colTwo.innerHTML = address;
+
+    var rowOne =  table.insertRow(3);
+   var colOne = rowOne.insertCell(0);
+   var colTwo = rowOne.insertCell(1);
+   colOne.innerHTML = "Toppings";
+  // alert(checkBoxValue.value);
+   colTwo.innerHTML = checkBoxValue;
+
+    var rowOne =  table.insertRow(4);
+   var colOne = rowOne.insertCell(0);
+   var colTwo = rowOne.insertCell(1);
+   colOne.innerHTML = "Delivery?:";
+   colTwo.innerHTML = delivery;
+
+    var rowOne =  table.insertRow(5);
+   var colOne = rowOne.insertCell(0);
+   var colTwo = rowOne.insertCell(1);
+   colOne.innerHTML = "Tip amount:";
+   colTwo.innerHTML = tip;
+
+    var rowOne =  table.insertRow(6);
+   var colOne = rowOne.insertCell(0);
+   var colTwo = rowOne.insertCell(1);
+   colOne.innerHTML = "Total:";
+   colTwo.innerHTML = "";
+   }*/
